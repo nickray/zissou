f49d32b08563f026d4717a62fe695f9a5e4ed06a
Nicolas Stalder <n@stalder.io>
2019-06-07 04:23:09 +0200

    git subrepo clone git@github.com:nickray/rust-littlefs.git
    subrepo:
      subdir:   "rust-littlefs"
      merged:   "7717ef3"
    upstream:
      origin:   "git@github.com:nickray/rust-littlefs.git"
      branch:   "master"
      commit:   "7717ef3"
    git-subrepo:
      version:  "0.4.0"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "5d6aba9"

---
+++ rust-littlefs/littlefs-sys/build.rs
@@ -0,0 +1,33 @@
+// Copyright 2018 by Brandon Edens.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+//
+// Author: Brandon Edens <brandonedens@gmail.com>
+// Date: 2018-11-10
+
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    cc::Build::new()
+        .flag("-std=c11")
+        .file("littlefs/lfs.c")
+        .file("littlefs/lfs_util.c")
+        .compile("lfs-sys");
+
+    let bindings = bindgen::Builder::default()
+        .header("littlefs/lfs.h")
+        .use_core()
+        .ctypes_prefix("cty")
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
